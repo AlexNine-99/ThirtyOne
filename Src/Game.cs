e102a5a5852ace39c1b7027ad7844d94460af613
AlexNine-99-proxy <alexnine99.proxy@gmail.com>
2025-05-15 08:22:48 -0700

    Use Src folder

---
+++ Src/Game.cs
@@ -0,0 +1,31 @@
+﻿using ThirtyOne.Src.Actors;
+
+namespace ThirtyOne.Src;
+
+internal class Game
+{
+    private readonly List<Player> _players;
+    private readonly Deck _deck = new();
+    private Player _currentPlayer;
+    private Player? _knocker;
+
+    public Game()
+    {
+        _players = [new("Player1"), new("Player2"), new("Player3")];
+        _currentPlayer = _players[0];
+    }
+
+    public void Start()
+    {
+        for (int i = 0; i < 3 * _players.Count; i ++)
+        {
+            _players[i % _players.Count].Cards.Add(_deck.Draw());
+        }
+        while (Progress());
+    }
+
+    private bool Progress()
+    {
+        return false;
+    }
+}
